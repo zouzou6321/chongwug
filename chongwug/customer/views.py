a2c2b04ab680b61d4ceecfb91e01d0a6324cf195
Wei Ren <wren@CNwren.local>
2014-08-20 15:32:02 +0800

    新建模版文件夹

--- chongwug/customer/views.py
+++ chongwug/customer/views.py
@@ -8,10 +8,10 @@ from django.utils import simplejson
 #date:2014/8/16
 #discription:导航页面展示
 def nav_page_view(request):
-    return render_to_response('nav.html')
+    return render_to_response('tpl/nav.html')
 
 #auth:huhuaiyong
 #date:2014/8/16
 #discription:购宠首页展示
 def buy_home_view(request):
-    return render_to_response('buy_index.html')
+    return render_to_response('tpl/buy_index.html')
