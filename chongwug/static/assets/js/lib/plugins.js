2bac02a025c3d1097ee8f0bbfb504991246b8125
Ren Wei <371151833@qq.com>
2014-09-24 17:54:39 +0800

    提取获取 url 参数的代码

--- chongwug/static/assets/js/lib/plugins.js
+++ chongwug/static/assets/js/lib/plugins.js
@@ -3000,3 +3000,28 @@ $.magnificPopup.registerModule(RETINA_NS, {
     }
 
 })(jQuery);
+
+(function($){
+    $.args = function (key){
+        var qs = (location.search.length > 0 ? location.search.substring(1) : ""),
+            args = {},
+            items = qs.length ? qs.split("&") : [],
+            item = null,
+            name = null,
+            value = null,
+            i = 0,
+            len = items.length;
+
+        for (i=0; i < len; i++){
+            item = items[i].split("=");
+            name = decodeURIComponent(item[0]);
+            value = decodeURIComponent(item[1]);
+
+            if (name.length){
+                args[name] = value;
+            }
+        }
+
+        return args[key] || args;
+    };
+})(jQuery);
