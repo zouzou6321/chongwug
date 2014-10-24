fe228dd9530f64b00a39626a3ba02de8bf401202
任为 <371151833@qq.com>
2014-10-24 15:23:08 +0800

    petfarm 静态文件移动

---
+++ chongwug/petfarm/templatetags/assets_petfarm.py
@@ -0,0 +1,45 @@
+import os, json, string
+from django import template
+
+register = template.Library()
+
+
+def process_json_file(json_file):
+    fin = open(json_file, 'r')
+    s = json.loads(fin.read())
+    fin.close()
+    return s
+
+
+@register.filter
+def assets_petfarm(value):
+    # config
+    root = '/static/'
+    curr = 'petfarm/'
+    flag = 'SERVER_SOFTWARE' in os.environ
+    cdn = False
+    prod = False
+    folder = '/static/petfarm/assets/dist/' if prod else 'assets/'
+    cdn_url = '//chongwug-cdn.b0.upaiyun.com' if flag or cdn else ''
+    js_version = '?v1'
+    css_version = '?v1'
+    img_version = '?v1'
+    version = ''
+    path = ''
+
+    if prod:
+        path = folder + value + version
+    else:
+        path = cdn_url + '/' + curr + folder + value + version
+
+    if value.endswith('.css'):
+        version = css_version
+    elif value.endswith('.js'):
+        version = js_version
+    else:
+        version = img_version
+
+    if flag or prod or cdn:
+        return path
+    else:
+        return root + curr + folder + 'dev/' + value + version
