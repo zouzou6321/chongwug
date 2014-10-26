516363d1a4b22155f1e6e09674b6d67bb7456490
胡怀勇 <692673390@qq.com>
2014-10-26 21:09:15 +0800

    修改人：胡怀勇 修改内容：1、实现芯片查询录入管理系统web端和应用程序端 影响功能：芯片注册

--- 宠物信息录入查询系统/宠物信息录入查询系统/Form1.cs
+++ 宠物信息录入查询系统/宠物信息录入查询系统/Form1.cs
@@ -55,10 +55,11 @@ namespace 宠物信息录入查询系统
             if (count >= 1000)
             {
                 MessageBox.Show("未检测到对端设备,点击确定退出程序");
-                //Environment.Exit(0);
+                Environment.Exit(0);
             }
-
+            
             InitializeComponent();
+            webBrowser1.ScriptErrorsSuppressed = true;
         }
 
         private string getIdNum(int index)
