7d520fe8963f3061685931b327cdd15332d646d8
胡怀勇 <692673390@qq.com>
2014-10-28 14:10:02 +0800

    修改人：胡怀勇 修改内容：1、重载session中间件，自动清除过期session，30分钟内持续操作重置超时时间 影响功能：图片上传

--- chongwug/chongwug/mysessionmiddleware.py
+++ chongwug/chongwug/mysessionmiddleware.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.contrib.sessions.middleware import SessionMiddleware
 from django.utils.importlib import import_module
+from django.utils import timezone
 class mySessionMiddleware(SessionMiddleware):
     def process_request(self, request):
         engine = import_module(settings.SESSION_ENGINE)
@@ -9,3 +10,8 @@ class mySessionMiddleware(SessionMiddleware):
         else:
             session_key = request.COOKIES.get(settings.SESSION_COOKIE_NAME, None)
         request.session = engine.SessionStore(session_key)
+        if settings.SESSION_ENGINE == 'django.contrib.sessions.backends.db':
+            Session.objects.filter(expire_date__lt=timezone.now()).delete()
+        request.session.set_expiry(60 * 30)
+        
+from django.contrib.sessions.models import Session
