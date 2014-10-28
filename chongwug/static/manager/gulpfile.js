53f48e2b0be68ed1cc27799b08e18b1207cc824e
任为 <371151833@qq.com>
2014-10-28 10:23:47 +0800

    manager 静态文件整理

--- chongwug/static/manager/gulpfile.js
+++ chongwug/static/manager/gulpfile.js
@@ -1,77 +1,103 @@
 var gulp = require('gulp'),
-    argv = require('minimist')(process.argv.slice(2)),
-    prod = argv.prod === true ? true : false,
     del = require('del'),
+    glob = require('glob'),
+    fs = require('fs'),
+    argv = require('minimist')(process.argv.slice(2)),
     plugins = require('gulp-load-plugins')();
 
+var config = require('./config.json'),
+    prod = argv.prod === true ? true : false,
+    dep = prod ? ['imgs'] : [];
 
-gulp.task('clean', function(){
-    del('assets/dev/**');
-});
 
-gulp.task('fonts', function(){
-    var stream = gulp.src('assets/src/fonts/**')
-        .pipe(gulp.dest('assets/dev/fonts'));
+gulp.task('css', dep, function(){
+    var map = JSON.parse(fs.readFileSync('./imgs-manifest.json')),
+        keys = Object.keys(map),
+        reg = new RegExp(keys.join('|'), 'gm');
 
-    if(prod){
-        stream.pipe(gulp.dest('assets/dist/fonts'));
-    }
-});
-
-gulp.task('css', function(){
-    var stream = gulp.src('assets/src/scss/**/*.scss')
+    gulp.src(config.css.src)
+        // compile sass
+//        .pipe(plugins.rubySass({
+//            noCache: true,
+//            sourcemap: false,
+//            style: 'expanded'
+//        }))
         .pipe(plugins.sass({
             precision: 8
         }))
-        .on( 'error', function(e){ console.log(e.toString()); })
-        .pipe(gulp.dest('assets/dev/css'));
-
-    if(prod){
-        stream.pipe(plugins.minifyCss({keepSpecialComments: 0, noAdvanced: true}))
-            .pipe(gulp.dest('assets/dist/css'));
-    }
+        .on( 'error', function(e){console.log(e)})
+//        .pipe(plugins.newer(config.css.dev))
+        //.pipe(plugins.autoprefixer())
+        //csscomb
+        //.pipe(plugins.csscomb())
+        // save to dev dir
+        //.pipe(plugins.cssshrink())
+//        .pipe(plugins.uncss({
+//            html: ['http://localhost:8000', 'http://localhost:8000/home']
+//        }))
+        .pipe(gulp.dest(config.css.dev))
+        //filter *.map
+        .pipe(plugins.if(prod, plugins.filter('**/*.css')))
+        .pipe(plugins.if(prod, plugins.replace(reg, function(e){ return map[e]; })))
+        //compress css
+        .pipe(plugins.if(prod, plugins.minifyCss({keepSpecialComments: 0, noAdvanced: true})))
+        //version css
+        .pipe(plugins.if(prod, plugins.rev()))
+        //save to prod dir
+        .pipe(plugins.if(prod, gulp.dest(config.css.prod)))
+        //generate manifest
+        .pipe(plugins.if(prod, plugins.rev.manifest({path: 'css-manifest.json'})))
+        //save manifest to prod dir
+        .pipe(plugins.if(prod, gulp.dest('./')));
 });
 
 gulp.task('js', function(){
-    var stream = gulp.src('assets/src/js/**')
-        .pipe(gulp.dest('assets/dev/js'));
-
-    if(prod){
-        stream.pipe(plugins.uglify())
-            .pipe(gulp.dest('assets/dist/js'));
-    }
+    gulp.src(config.js.src)
+        .pipe(gulp.dest(config.js.dev))
+        .pipe(plugins.if(prod, plugins.uglify()))
+        .on('error', function(e){
+            console.log(e.toString());
+        })
+        .pipe(plugins.if(prod, plugins.rev()))
+        .pipe(plugins.if(prod, gulp.dest(config.js.prod)))
+        .pipe(plugins.if(prod, plugins.rev.manifest({path: 'js-manifest.json'})))
+        .pipe(plugins.if(prod, gulp.dest('./')));
 });
 
 gulp.task('imgs', function(){
-    var stream = gulp.src('assets/src/imgs/**/*.+(jpg|png|gif)')
-        .pipe(gulp.dest('assets/dev/imgs'));
+    gulp.src(config.imgs.src)
+        .pipe(gulp.dest(config.imgs.dev))
+        .pipe(plugins.if(prod, plugins.imagemin()))
+        .pipe(plugins.if(prod, plugins.rev()))
+        .pipe(plugins.if(prod, gulp.dest(config.imgs.prod)))
+        .on('error', function(e){ console.log(e); })
+        .pipe(plugins.if(prod, plugins.rev.manifest({path: 'imgs-manifest.json'})))
+        .pipe(plugins.if(prod, gulp.dest('./')));
+});
 
-    if(prod){
-        stream.pipe(plugins.imagemin())
-            .pipe(gulp.dest('assets/dist/imgs'))
-    }
+gulp.task('clean', function(){
+    del('dev/**');
+});
+
+gulp.task('default', function(){
+    gulp.start(['css', 'js', 'imgs']);
 });
 
 gulp.task('vendor', function(){
-    var css = plugins.filter('**/*.css'),
-        js = plugins.filter(['**/*.js', '!ckeditor/**']);
-
-    var stream = gulp.src('assets/src/vendor/**')
-        .pipe(gulp.dest('assets/dev/vendor'));
-
-    if(prod){
-        stream.pipe(css)
-            .pipe(plugins.minifyCss({keepSpecialComments: 0, noAdvanced: true}))
-            .pipe(css.restore())
-            .pipe(js)
-            .pipe(plugins.uglify())
-            .pipe(js.restore())
-            .pipe(gulp.dest('assets/dist/vendor'));
-    }
+   gulp.src('./src/vendor/**', {base: './src'})
+       .pipe(gulp.dest('./dev'))
+       .pipe(plugins.if(prod, gulp.dest('./dist')));
 });
+
 
 gulp.task('watch', function(){
-    gulp.watch('assets/src/scss/**/*.scss', ['css']);
-});
+    plugins.watch({
+        glob: config.imgs.src,
+        emitOnGlob: false,
+        passThrough: false
+    }, function(files) {
+        gulp.start('imgs');
+    });
 
-gulp.task('default', ['js', 'css', 'imgs', 'fonts', 'vendor']);
+    gulp.watch(config.css.src, ['css']);
+});
