fdbeb9264e7f1b395496df74c05bf46fe9f3b88c
胡怀勇 <692673390@qq.com>
2014-10-28 11:29:30 +0800

    修改人：胡怀勇 修改内容：1、重载session中间件，支持从post传递过来的sessionid 影响功能：图片上传

--- chongwug/chongwug/mysessionmiddleware.py
+++ chongwug/chongwug/mysessionmiddleware.py
@@ -4,9 +4,8 @@ from django.utils.importlib import import_module
 class mySessionMiddleware(SessionMiddleware):
     def process_request(self, request):
         engine = import_module(settings.SESSION_ENGINE)
-        session_key = request.COOKIES.get(settings.SESSION_COOKIE_NAME, None)
-        if not session_key:
-            if 'sessionid' in request.POST:
-                session_key = request.POST['sessionid']
-                print request.POST['sessionid']
+        if 'sessionid' in request.POST:
+            session_key = request.POST['sessionid']
+        else:
+            session_key = request.COOKIES.get(settings.SESSION_COOKIE_NAME, None)
         request.session = engine.SessionStore(session_key)
