facfc94a6d19dae2276fcd7f66c2d11d7b1af4ef
胡怀勇 <692673390@qq.com>
2014-10-31 10:10:07 +0800

    修改人：胡怀勇 修改内容：1、宠物疫苗首页定向到查询页 影响功能：芯片系统

--- petlife/petlife/urls.py
+++ petlife/petlife/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from settings import STATIC_ROOT
+from django.views.generic import RedirectView
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+                       url(r'^$', RedirectView.as_view(url='/petmicro/')),
                        url(r'^petmicro/', include('petmicro.urls')),
                        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': STATIC_ROOT,'show_indexes':False  }),
                        url(r'^admin/', include(admin.site.urls)),
