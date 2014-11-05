9cdd4639194eaf81c50e31fbc3814ba88ce6f7ba
胡怀勇 <692673390@qq.com>
2014-11-05 11:24:30 +0800

    修改人：胡怀勇 修改内容：1、页面适配 影响功能：市场

--- chongwug/marketer/urls.py
+++ chongwug/marketer/urls.py
@@ -1,6 +1,6 @@
 # -*- coding: UTF-8 -*-
 from django.conf.urls import patterns, url
-from views import market_attention_data_view,manage_home_view,market_usr_attention_info_view,market_usr_attention_mod_view,market_nestofpet_info_view,market_usr_processed_info_view,market_usr_untreated_info_view,market_usr_fail_info_view,market_usr_success_info_view
+from views import market_usr_attention_mod_select_view,market_attention_data_view,manage_home_view,market_usr_attention_info_view,market_usr_attention_mod_view,market_nestofpet_info_view,market_usr_processed_info_view,market_usr_untreated_info_view,market_usr_fail_info_view,market_usr_success_info_view
 
 urlpatterns = patterns(
                        '',
@@ -12,5 +12,6 @@ urlpatterns = patterns(
                        url(r'^success/$',market_usr_success_info_view),
                        url(r'^attention/data/$', market_attention_data_view),
                        url(r'^attention/mod/$',market_usr_attention_mod_view),
+                       url(r'^attention/mod/select/$',market_usr_attention_mod_select_view),
                        url(r'^nestofpet/$',market_nestofpet_info_view),
                        )
