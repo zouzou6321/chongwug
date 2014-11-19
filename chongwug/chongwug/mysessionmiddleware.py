f156c8e36221a1e58a4cd0a38f05c375bdda4782
胡怀勇 <692673390@qq.com>
2014-11-19 12:47:35 +0800

    修改人：胡怀勇 修改内容：1、bug修复 影响功能：养殖场

--- chongwug/chongwug/mysessionmiddleware.py
+++ chongwug/chongwug/mysessionmiddleware.py
@@ -5,10 +5,13 @@ from django.utils import timezone
 class mySessionMiddleware(SessionMiddleware):
     def process_request(self, request):
         engine = import_module(settings.SESSION_ENGINE)
-        if 'sessionid' in request.POST:
-            session_key = request.POST['sessionid']
-        else:
-            session_key = request.COOKIES.get(settings.SESSION_COOKIE_NAME, None)
+        session_key = request.COOKIES.get(settings.SESSION_COOKIE_NAME, None)
+        if session_key == None:
+            try:
+                if settings.SESSION_COOKIE_NAME in request.POST:
+                    session_key = request.POST[settings.SESSION_COOKIE_NAME]
+            except:
+                pass
         request.session = engine.SessionStore(session_key)
         if settings.SESSION_ENGINE == 'django.contrib.sessions.backends.db':
             Session.objects.filter(expire_date__lt=timezone.now()).delete()
