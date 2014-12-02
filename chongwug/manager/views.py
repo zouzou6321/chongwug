bbd851668e04cf56a9a81be42db01b94835bf04b
胡怀勇 <692673390@qq.com>
2014-12-02 10:01:10 +0800

    修改人：胡怀勇 修改内容：1、宠物知识修改后台搭建 影响功能：消费者

--- chongwug/manager/views.py
+++ chongwug/manager/views.py
@@ -139,3 +139,10 @@ def manage_supplie_mod_view(request):
     else:
         data['supplietype'],data['supplies'] = adapters.manage_get_supplies(request)
         return render_to_response('manager/tpl/manage_supplie_mod.html',data,context_instance=RequestContext(request))
+
+def manage_knowledge_mode_view(request):
+    if adapters.manage_authentication(request) == False:
+        return HttpResponseRedirect(MANAGE_ROOT)
+    if request.session['score'] < 50:
+        return render_to_response('404.html')
+    return HttpResponse('True')
