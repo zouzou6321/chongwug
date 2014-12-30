eb73158ca68cbd00dde5b425e5defa1722d85370
胡怀勇 <692673390@qq.com>
2014-12-30 15:49:03 +0800

    修改人：胡怀勇 修改内容：1、添加图形验证码 影响功能：养殖场

--- chongwug/petfarm/urls.py
+++ chongwug/petfarm/urls.py
@@ -1,12 +1,13 @@
 # -*- coding: UTF-8 -*-
 from django.conf.urls import patterns, url
 from petfarm.views import manage_home_view,manage_pet_farm_mod_view,manage_pet_farm_pic_upload_view,manage_nestofpet_add_view
-from petfarm.views import manage_pwdforgot_view,manage_regist_picadd_view,manage_regist_view,address_handle_view,manage_nestofpet_mod_view,manage_nestofpet_pic_upload_view
+from petfarm.views import manage_validate_view,manage_pwdforgot_view,manage_regist_picadd_view,manage_regist_view,address_handle_view,manage_nestofpet_mod_view,manage_nestofpet_pic_upload_view
 urlpatterns = patterns(
                        '',
                        url(r'^$',manage_home_view),
                        url(r'^regist/$',manage_regist_view),
                        url(r'^regist/picadd/$',manage_regist_picadd_view),
+                       url(r'^validate/$', manage_validate_view),
                        url(r'^pwdforgot/$',manage_pwdforgot_view),
                        url(r'^nestofpet/add/$',manage_nestofpet_add_view),
                        url(r'^nestofpet/mod/$',manage_nestofpet_mod_view),
